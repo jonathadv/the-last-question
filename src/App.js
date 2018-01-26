227a1bbbcd19d9bc9bab76aa126a5d3a1b1e4b8e
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2018-01-25 23:48:05 -0200

    Improving css; removing React logos

--- src/App.js
+++ src/App.js
@@ -9,12 +9,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">The Last Question</h1>
         </header>
             <Countdown targetDate={new Date(2061,4,21)}/>
         <p className="counter-text">
-            are left until <a href="https://en.wikipedia.org/wiki/The_Last_Question" target="_blank"><i>The Last Question</i></a> is asked for the first time.
+            are left until <a href="https://en.wikipedia.org/wiki/The_Last_Question" target="_blank"><i class="no-underline">The Last Question</i></a> is asked for the first time.
         </p>
         <p class="quote">
           "The last question was asked for the first time, half in jest, on May 21, 2061, at a time when humanity first stepped in to the light."
