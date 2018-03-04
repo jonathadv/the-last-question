63925f4f98cba94578d61f4bf8247ae76f97c3ca
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2018-03-04 00:56:50 -0300

    Improving layout

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">The Last Question</h1>
+
+        <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet"/>
+
+          <h1 className="App-title">> THE LAST QUESTION</h1>
+
         </header>
             <Countdown targetDate={new Date(2061,4,21)}/>
-        <p className="counter-text">
+            <p className="counter-text">
             are left until <a href="https://en.wikipedia.org/wiki/The_Last_Question" target="_blank"><i class="no-underline">The Last Question</i></a> is asked for the first time.
         </p>
-        <p class="quote">
+        <p className="quote">
           "The last question was asked for the first time, half in jest, on May 21, 2061, at a time when humanity first stepped in to the light."
         </p>
-
+          <p className="answer">INSUFFICIENT DATA FOR MEANINGFUL ANSWER.<span className="blink">|</span></p>
       </div>
     );
   }
